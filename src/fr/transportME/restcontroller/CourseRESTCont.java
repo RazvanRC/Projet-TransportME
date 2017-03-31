4aa40b01959b19cca8067921947d04dd5dcfa978
sydul25 <sdsydul@gmail.com>
2017-03-31 10:50:27 +0200

    complement de service

--- src/fr/transportME/restcontroller/CourseRESTCont.java
+++ src/fr/transportME/restcontroller/CourseRESTCont.java
@@ -1,6 +1,6 @@
 package fr.transportME.restcontroller;
 
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.NoResultException;
@@ -17,8 +17,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.transportME.DAO.ConducteurDAO;
 import fr.transportME.DAO.CourseDAO;
 import fr.transportME.model.Client;
+import fr.transportME.model.Conducteur;
 import fr.transportME.model.Course;
 
 @RestController
@@ -27,9 +29,12 @@ public class CourseRESTCont {
 	
 	@Autowired
 	private CourseDAO courseDAO;
+	
+	@Autowired
+	private ConducteurDAO conducteurDAO;
 
 	/**
-	 * methode pour la commande d'une course
+	 * methode pour la commande d'une course (creation)
 	 * @param course
 	 * @param bindingResult
 	 * @return
@@ -68,22 +73,55 @@ public class CourseRESTCont {
 	/**
 	 * methode pour demarrer une course
 	 * @param idCourse
-	 * @param dateDepart
 	 * @return
 	 */
-	@RequestMapping(value="/{idCourse}/demarrer", method= RequestMethod.POST)
+	@RequestMapping(value="/{idCourse}/demarrer", method= RequestMethod.PUT)
 	@ResponseBody
-	public ResponseEntity<Course> demarrerCourse(@PathVariable("idCourse") int idCourse, 
-			@RequestParam("dateDepart") Date dateDepart) {
+	public ResponseEntity<Course> demarrerCourse(@PathVariable("idCourse") int idCourse) {
 		
 		System.out.println("demarrage de la course idCourse="+idCourse);
 		Course myCourse = this.courseDAO.find(idCourse);
-		System.out.println("date de depart="+dateDepart);
-		myCourse.setDateDepart(dateDepart);
+		
+		// on rend indisponible le conducteur associé à la course
+		Conducteur myConducteur = myCourse.getConducteur();
+		System.out.println("id utilisateur "+myConducteur.getIdUtil());
+		System.out.println("Telephone "+myConducteur.getTelephoneUtil());
+		myConducteur.setStatut(false);
+		conducteurDAO.save(myConducteur);
+		
+		myCourse.setDateDepart(new Date());
 		
 		System.out.println("sauvegarde");
 		return new ResponseEntity<Course>(courseDAO.save(myCourse),	HttpStatus.OK);
 	}
+	
+	/**
+	 * methode pour terminer une course
+	 * @param idCourse
+	 * @return
+	 */
+	@RequestMapping(value="/{idCourse}/terminer", method= RequestMethod.PUT)
+	@ResponseBody
+	public ResponseEntity<Course> terminerCourse(@PathVariable("idCourse") int idCourse) {
+		
+		System.out.println("terminer la course idCourse="+idCourse);
+		Course myCourse = this.courseDAO.find(idCourse);
+		myCourse.setPrixEstime(15.00f);
+		
+		// on rend disponible le conducteur associé à la course
+		Conducteur myConducteur = myCourse.getConducteur();
+		myConducteur.setStatut(true);
+		conducteurDAO.save(myConducteur);
+
+		Date dt = new Date();
+		System.out.println("date "+dt);
+		myCourse.setDateArrivee(dt);
+		
+		System.out.println("sauvegarde");
+		return new ResponseEntity<Course>(courseDAO.save(myCourse),	HttpStatus.OK);
+	}
+	
+
 	
 	
 	/**
